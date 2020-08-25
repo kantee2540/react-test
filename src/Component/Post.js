bc2febf0dc628c994f481b674a635f4d40109270
DEV wsmart <nongnapat.j@wsmart.co.th>
2020-08-25 13:41:35 +0700

    can post but value is still null

--- src/Component/Post.js
+++ src/Component/Post.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button, Modal } from 'react-bootstrap';
 import axios from 'axios';
 import './Post.css'
 
@@ -15,7 +16,7 @@ class Post extends Component{
     }
 
     componentDidMount(){
-        axios.get("http://localhost:8080/Post")
+        axios.get("http://localhost:8000/post")
         .then(response => {
             this.setState({
                 isLoaded: true,
@@ -30,17 +31,11 @@ class Post extends Component{
         });
     }
 
-    createPost() {
-        
-    }
-
     render(){
         const { error, isLoaded, items } = this.state
         return(
             <div>
-                <a className="float-button" href="#" data-toggle="modal" data-target="#createpost">
-                    <i className="fas fa-pen"></i>
-                </a>
+                
                 {!isLoaded ? <Loading/>: ''}
                 <h1>Post</h1>
                 <div className="row">
@@ -60,37 +55,78 @@ class Post extends Component{
                     ))}
                 </div>
 
-                <div className="modal fade" id="createpost" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-                <div className="modal-dialog" role="document">
-                    <div className="modal-content">
-                    <div className="modal-header">
-                        <h5 className="modal-title" id="exampleModalLabel">Create post</h5>
-                        <button type="button" className="close" data-dismiss="modal" aria-label="Close">
-                        <span aria-hidden="true">&times;</span>
-                        </button>
-                    </div>
-                    <div className="modal-body">
-                        <textarea className="post-textarea form-control" rows="5" placeholder="Say something..."></textarea>
-                    </div>
-                    <div className="modal-footer">
-                        <button type="button" className="btn btn-secondary" data-dismiss="modal">Discard</button>
-                        <button type="button" className="btn btn-primary">Post</button>
-                    </div>
-                    </div>
-                </div>
-                </div>
+                <PostModal/>
 
             </div>
         )
     }
+
+    
 }
 
 export default Post;
 
-class Loading extends Post{
-    render(){
-        return(
-            <div className="loading">Loading</div>
-        )
+function Loading(){
+    return(
+        <div className="loading">Loading</div>
+    )
+    
+}
+
+function PostModal(){
+
+    var message = "";
+    const [show, setShow] = React.useState(false);
+
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
+
+    const postMessage = () => {
+        var mes = message;
+        console.log(mes);
+        axios.post("http://localhost:8000/post/add", {
+            message: mes
+            
+        },{
+            headers: {
+                'Content-Type': 'application/x-www-form-urlencoded'
+            }
+        })
+        .then(response => {
+            if (response.status == 200){
+                handleClose();
+            }
+        }).catch(error =>{
+            console.log(error);
+        });
     }
+
+    const handleUpdatevalue = (event) => {
+        message = event.target.value;
+    }
+    
+    return(
+        <>
+            <a className="float-button" href="#" onClick={handleShow}>
+                <i className="fas fa-pen"></i>
+            </a>
+
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                <Modal.Title>Post Message</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <textarea className="post-textarea form-control" rows="5" onChange={handleUpdatevalue.bind(this)} placeholder="Say something..."></textarea>
+                </Modal.Body>
+                <Modal.Footer>
+                <Button variant="secondary" onClick={handleClose}>
+                    Discard
+                </Button>
+                <Button variant="primary" onClick={postMessage}>
+                    Post
+                </Button>
+                </Modal.Footer>
+            </Modal>
+        </>
+    )
 }
